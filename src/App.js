922f0cec2ed3ca3cce1cbd2d7d1635dec9738e48
Jake Ma4o <2000_aktau@mail.ru>
2022-09-21 04:16:30 +0500

    Tenzies

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react"
+import Die from "./components/DIe"
+import { nanoid } from "nanoid"
+import Confetti from "react-confetti"
 
 function App() {
+  // check out the lazy state so when update the state it doesnt render shit again
+  const [dice, setDice] = useState(allNewDice());
+
+  const [tenzies, setTenzies] = useState(false);
+
+  useEffect(() => {
+    const allHeld = dice.every(die => die.isHeld);
+    const firstValue = dice[0].value;
+    const allSameValue = dice.every(die => die.value === firstValue);
+    if (allHeld && allSameValue) {
+      setTenzies(true)
+    }
+  }, [dice])
+
+  function generateNewDie() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid()
+    }
+  }
+
+  function allNewDice() {
+    const newDice = []
+    for (let i = 0; i < 10; i++) {
+      newDice.push(generateNewDie())
+    }
+    return newDice
+  }
+
+  function rollAllDices() {
+    if (!tenzies) {
+      setDice(prevDice => prevDice.map(die => {
+        return die.isHeld ? die : generateNewDie()
+      }))
+    } else {
+      setTenzies(false)
+      setDice(allNewDice())
+    }
+  }
+
+  function holdDice(id) {
+    setDice(prevDices => prevDices.map(die => {
+      return die.id === id ? { ...die, isHeld: !die.isHeld } : die
+    })
+    )
+  }
+
+
+  const diceElements = dice.map(die => <Die key={die.id} id={die.id} value={die.value} isHeld={die.isHeld} holdDice={() => holdDice(die.id)} />)
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <main>
+          {tenzies && <Confetti />}
+          <h1 className="title">Tenzies</h1>
+          <p className="instruction">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+          <div className="game-field">
+            {diceElements}
+          </div>
+          <button onClick={rollAllDices}>{tenzies ? "New Game" : "Roll"}</button>
+        </main>
+      </div>
     </div>
   );
 }
---
+++ src/components/DIe.js
@@ -0,0 +1,11 @@
+export default function Die(props) {
+  return (
+    <div
+      className="die"
+      style={{ backgroundColor: props.isHeld && "#59E391" }}
+      onClick={props.holdDice}
+    >
+      <h2>{props.value}</h2>
+    </div>
+  )
+}
